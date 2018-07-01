4f65e73524fb3b7eb210747401fb2ecc6db3c88b
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2018-07-02 07:47:15 +0800

    Update elsl.ext.pdv.js

--- pre1/elsl.ext.pdv.js
+++ pre1/elsl.ext.pdv.js
@@ -1,2 +1,8 @@
-function PreDefinedValue(id, description) {
+function PreDefinedValue(description) {
+	this.id = Math.floor(Math.random() * 65536) + 1;
+	this.description = description;
+}
+
+function _pdv(desc) {
+	return new PreDefinedValue(desc);
 }
