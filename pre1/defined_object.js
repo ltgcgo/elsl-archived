c1431d72f33883022fc738395e8cfa35703b4154
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2018-07-23 15:50:25 +0800

    Update and rename elsl.ext.pdv.js to defined_object.js

--- pre1/defined_object.js
+++ pre1/defined_object.js
@@ -1,8 +1,8 @@
-function PreDefinedValue(description) {
+function DefinedObject(description) {
 	this.id = Math.floor(Math.random() * 65536) + 1;
 	this.description = description;
 }
 
-function _pdv(desc) {
+function _do(desc) {
 	return new PreDefinedValue(desc);
 }
