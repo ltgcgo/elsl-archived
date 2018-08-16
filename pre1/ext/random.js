b5ebdc279cd9e73bb8d1f84bd5a200357fd631f2
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2018-08-16 13:49:50 +0800

    Update random.js

--- pre1/ext/random.js
+++ pre1/ext/random.js
@@ -2,6 +2,7 @@
 function Random() {
 	this._seed = 1;
 	this._list = [];
+	this._count = 0;
 	this.seed = function (seed) {
 		if (seed > 1 && seed < 4294967255 && seed % 1 == 0) {
 			this._seed = seed;
@@ -22,7 +23,8 @@ function Random() {
 	this.get = function () {
 		let key1 = this._seed / 100000 % 1;
 		let key2 = this._seed % 100000;
-		let random = Math.floor(0.5 * Math.sin(key2 * now - key1) + 0.5);
+		let random = Math.floor(0.5 * Math.sin(key2 * (this._count + 1) - key1) + 0.5);
 		console.log(key1 + " | " + key2);
+		this._count ++;
 	}
 }
