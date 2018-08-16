53654d220dca9451665e4b5002ac9f15f46c2f70
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2018-08-16 14:08:54 +0800

    Update random.js

--- pre1/ext/random.js
+++ pre1/ext/random.js
@@ -12,9 +12,14 @@ function Random() {
 		}
 	}
 	this.open = function () {
+		let count = 0;
+		while (count < this.range.e - this.range.s) {
+			this._list[count] = count + this.range.s;
+			count ++;
+		}
 	}
 	this.range = function (start, end) {
-		if (start.constructor == Number && end.constructor == Number) {
+		if (start.constructor == Number && end.constructor == Number && start <= end) {
 			this.range.s = start;
 			this.range.e = end;
 		} else {
@@ -22,10 +27,16 @@ function Random() {
 		}
 	}
 	this.get = function () {
-		let key1 = Math.floor(this._seed / 100000);
-		let key2 = this._seed % 100000;
+		let key1 = Math.floor(this._seed / 100000) / 100;
+		let key2 = this._seed % 100000 / 100;
 		let random = 0.5 * Math.sin(key2 * (this._count + 1) - key1) + 0.5;
-		console.log(key1 + " | " + key2 + " | " + random);
-		this._count ++;
+		if (this._list != []) {
+			let output = this._list[Math.floor(random * _list.length)];
+			this._list.splice(Math.floor(random * _list.length), 1);
+			this._count ++;
+			return output;
+		} else {
+			console.error("[elsl.ext.random] Use .open() to refresh the random list!");
+		}
 	}
 }
