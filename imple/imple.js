35750b914ba6bc9ec309e4e59334f2ddfa299a25
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2018-08-16 14:29:01 +0800

    Update imple.js

--- imple/imple.js
+++ imple/imple.js
@@ -1,8 +1,8 @@
 function useCanvas() {
 	let canvas = document.getElementsByTagName("canvas")[0] || document.createElement("canvas");
 	canvas.style = "position: absolute; top: 0; left: 0; width: 100%; height: 100%; box-shadow: inset 0px 0px 4px 2px #111";
-	canvas.width = window.outerWidth;
-	canvas.height = window.outerHeight;
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
 	document.body.appendChild(canvas);
 	return canvas.getContext("2d");
 }
