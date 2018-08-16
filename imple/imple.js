f375a99618efc5c23f18830975b4a4ebbc5cbc90
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2018-08-16 14:17:51 +0800

    Update imple.js

--- imple/imple.js
+++ imple/imple.js
@@ -1,5 +1,5 @@
 function useCanvas() {
-	let canvas = docudocument.getElementsByTagName("canvas")[0] || document.createElement("canvas");
+	let canvas = document.getElementsByTagName("canvas")[0] || document.createElement("canvas");
 	canvas.style = "position: absolute; top: 0; left: 0; width: 100%; height: 100%; box-shadow: 0 0 1px 1px #ddd;";
 	document.body.appendChild(canvas);
 	return canvas.getContext("2d");
